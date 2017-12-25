4afc064125f7c99566e4f619151d258e183e7e04
shohkhan <shoaibapece@gmail.com>
2017-12-24 22:15:44 -0600

    Update enc.sh

--- enc.sh
+++ enc.sh
@@ -8,6 +8,7 @@ tar --lzip -xvf gmp
 cd gmp-6.1.2
 ./configure
 make
+make check
 sudo make install
 
 cd ..
@@ -16,8 +17,12 @@ wget http://www.shoup.net/ntl/ntl-10.5.0.tar.gz -O ntl
 tar -xvf ntl
 #cd ntl-10.3.0/src/
 cd ntl-10.5.0/src/
-./configure NTL_GMP_LIP=on
+./configure NTL_GMP_LIP=on NTL_EXCEPTIONS=on
+# While making cuhe, if you get the error: /usr/bin/ld: /usr/local/lib/libntl.a(ZZ.o): relocation R_X86_64_32S against `.rodata' can not be used when making a shared object; recompile with -fPIC
+# Edit the makefile and include the flag -fPIC in the CXXFLAGS variable
+# Helpful: https://stackoverflow.com/questions/19364969/compilation-fails-with-relocation-r-x86-64-32-against-rodata-str1-8-can-not
 make
+make check
 sudo make install
 
 cd ..
@@ -27,3 +32,10 @@ unzip HElib.zip
 cd HElib-master/src/
 make
 
+cd ..
+cd ..
+
+cd chHE/cuhe
+# Remember to use an existing gcc on CMake file
+cmake ./
+make
